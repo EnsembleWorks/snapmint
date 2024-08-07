9d6bf3ced801e107da3e2dc7b0cc8257e4eab4c6
Jayden Boesch <54635973+jaboesch@users.noreply.github.com>
2024-08-07 15:24:48 -0400

    Make the mock text larger

--- src/captureManager.py
+++ src/captureManager.py
@@ -53,31 +53,45 @@ class CaptureManager:
     def get_current_unix_time(self):
         return int(time.time())
 
-
     def mock_read(self):
         # Create a blank black image
         height, width = 2160, 3840  # You can adjust these dimensions to your needs
         frame = np.zeros((height, width, 3), dtype=np.uint8)
 
         # Get current timestamp
-        timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        timestamp1 = datetime.now().strftime("%Y-%m-%d")
+        timestamp2 = datetime.now().strftime("%H:%M:%S")
 
         # Set font scale and thickness
-        font_scale = 8
-        thickness = 4
+        font_scale = 15
+        thickness = 12
 
         # Calculate the text size to center the timestamp
-        text_size = cv2.getTextSize(
-            timestamp, cv2.FONT_HERSHEY_SIMPLEX, font_scale, thickness
+        text_size1 = cv2.getTextSize(
+            timestamp1, cv2.FONT_HERSHEY_SIMPLEX, font_scale, thickness
         )[0]
-        text_x = (width - text_size[0]) // 2
-        text_y = (height + text_size[1]) // 2
+        text_size2 = cv2.getTextSize(
+            timestamp2, cv2.FONT_HERSHEY_SIMPLEX, font_scale, thickness
+        )[0]
+        text_x1 = (width - text_size1[0]) // 2
+        text_y1 = ((height + text_size1[1]) // 2) - text_size2[1]
+        text_x2 = (width - text_size2[0]) // 2
+        text_y2 = ((height + text_size1[1]) // 2) + text_size2[1]
 
         # Put timestamp on the frame
         cv2.putText(
             frame,
-            timestamp,
-            (text_x, text_y),
+            timestamp1,
+            (text_x1, text_y1),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            font_scale,
+            (255, 255, 255),
+            thickness,
+        )
+        cv2.putText(
+            frame,
+            timestamp2,
+            (text_x2, text_y2),
             cv2.FONT_HERSHEY_SIMPLEX,
             font_scale,
             (255, 255, 255),
@@ -90,7 +104,7 @@ class CaptureManager:
     def perform_capture(self):
         # ret, frame = self.cap.read()
         ret, frame = self.mock_read()
-        
+
         # print(".", end="", flush=True)
         captureTime = self.get_current_unix_time()
         if ret:
